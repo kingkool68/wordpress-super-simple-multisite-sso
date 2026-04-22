8e4ae1b9a3bea176ca9009a14f5f6aa37b11767b
Russell Heimlich <kingkool68@gmail.com>
2026-04-22 01:13:02 -0400

    Add default endpoint values

--- class-oidc-settings-override.php
+++ class-oidc-settings-override.php
@@ -27,20 +27,31 @@ class OIDC_Settings_Override {
 			$settings = array();
 		}
 
+		// Get the current site ID.
 		$blog_id = get_current_blog_id();
 
+		// Bail if we're on the defined hub site.
 		if ( (int) SS_MS_SSO_HUB_SITE_ID === (int) $blog_id ) {
 			return $settings;
 		}
 
-		$client_id = 'client_' . $blog_id;
-		$salt      = defined( 'AUTH_KEY' ) ? AUTH_KEY : 'fallback-oidc-secret-salt';
-		$secret    = hash_hmac( 'sha256', $blog_id, $salt );
+		// Get the Hub Site URL.
+		$hub_site_url = get_site_url( (int) SS_MS_SSO_HUB_SITE_ID );
+		if ( empty( $hub_site_url ) ) {
+			return $settings;
+		}
+		$hub_site_url = untrailingslashit( $hub_site_url );
+
+		$client_id      = 'client_' . $blog_id;
+		$client_configs = Client_Sites::get_clients();
 
 		$overrides = array(
-			'client_id'     => $client_id,
-			'client_secret' => $secret,
-			// 'endpoint_jwks' => '', // Should be /.well-known/jwks.json once https://github.com/Automattic/wp-openid-connect-server/pull/132 is merged
+			'client_id'         => $client_id,
+			'client_secret'     => $client_configs[ $client_id ]['secret'] ?? '',
+			'endpoint_login'    => $hub_site_url . '/wp-json/openid-connect/authorize',
+			'endpoint_userinfo' => $hub_site_url . '/wp-json/openid-connect/userinfo',
+			'endpoint_token'    => $hub_site_url . '/wp-json/openid-connect/token',
+			'endpoint_jwks'     => $hub_site_url . '/.well-known/jwks.json', // Should be /.well-known/jwks.json once https://github.com/Automattic/wp-openid-connect-server/pull/132 is merged
 		);
 
 		/**
@@ -50,6 +61,8 @@ class OIDC_Settings_Override {
 		 * @param int   $blog_id   The current site's blog ID.
 		 */
 		$overrides = apply_filters( 'ss_ms_sso_oidc_settings_overrides', $overrides, $blog_id );
+
+		var_dump( $overrides );
 
 		return array_merge( $settings, $overrides );
 	}
